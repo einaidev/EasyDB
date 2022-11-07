1ccf93a4bb188d8fe8d446b08f492d3b35124860
Einai <81778449+einaidev@users.noreply.github.com>
2022-11-07 18:18:04 -0300

    Update loadJson.py

--- examples/loadJson.py
+++ examples/loadJson.py
@@ -2,5 +2,5 @@ from EasyDB import EasyDB,DB
 
 collection:DB = EasyDB().newCollection
 
-collection.load("<path or file stream>", mode="add")
+collection.load("<path or file stream>", mode="addppend")
 #collection.load("<path or file stream>", mode "append"|"set")
